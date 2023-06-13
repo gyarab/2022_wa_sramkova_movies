ff077fd1a08f8aba5a6fccb53900bd6471780c97
janag162 <100915572+janag162@users.noreply.github.com>
2023-06-13 09:14:49 +0200

    Upload file

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
-from .models import Movie, Director, Actor
+from .models import Movie, Director, Actor, Genre, Comment
+from django.db.models import Q
+from .forms import CommentForm
 
 def directors(request):
     context = {
         'title': "Herci",
         'directors': Director.objects.all()
     }
+    print(context)
     return render(request, 'directors.html', context)
 
 
 def homepage(request): #defines what is seen on the first page after executing runserver, has to be pathed in urls.py
     context = {
-        "movies": Movie.objects.all()
+        "movies": Movie.objects.all(),
+        "actors": Actor.objects.all(),
+        "directors": Director.objects.all(),
+        "genres": Genre.objects.all(),
     }
     return render(request, 'main.html', context)
 
@@ -22,14 +28,46 @@ def director(request, id):
     return render(request, 'director.html', context)
 
 def movies(request):
+    movies_queryset = Movie.objects.all()
+    genre = request.GET.get('genre')
+    if genre:
+        movies_queryset = movies_queryset.filter(genres__name=genre)
+    search = request.GET.get('search')
+    if search:
+        movies_queryset = movies_queryset.filter(Q(name__icontains=search)|Q(description__icontains=search)) 
+
     context = {
-        "movies": Movie.objects.all()
+        "movies": movies_queryset,
+        "genres": Genre.objects.all().order_by('name'),
+        "genre": genre,
+        "search": search,
     }
     return render(request, 'movies.html', context)
 
 def movie(request, id):
+    m = Movie.objects.get(id=id)
+    f = CommentForm()
+
+    if request.POST:
+        f = CommentForm(request.POST)
+        if f.is_valid():
+            # ulozit do DB
+            c = Comment(
+                movie=m,
+                author=f.cleaned_data.get('author'),
+                text=f.cleaned_data.get('text'),
+                rating=f.cleaned_data.get('rating'),
+            )
+            if not c.author:
+                c.author = 'Anonym'
+            c.save()
+            # nastavit prazdny form
+            f = CommentForm()
+
     context = {
-        "movie": Movie.objects.get(id=id)
+        "movie": m,
+        "comments": Comment.objects.filter(movie=m).order_by('-created_at'),
+        "form": f
     }
     return render(request, 'movie.html', context)
 
@@ -42,5 +80,7 @@ def actors(request):
 def actor(request, id):
     context = {
         "actor": Actor.objects.get(id=id)
+    }
+    return render(request, 'actor.html', context)
     }
     return render(request, 'actor.html', context)
