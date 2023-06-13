c4d54d92906fd1c11a999b3bbc50b66feea256b8
janag162 <100915572+janag162@users.noreply.github.com>
2023-06-13 08:59:23 +0200

    Update admin.py

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Movie, Director, Genre
+from .models import Movie, Director, Genre, Actor, Comment
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'year', 'director', 'genres_display'] #defines what is seen in the overall table
@@ -8,16 +8,16 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_filter = ['genres', 'year']
 
-admin.site.register(Movie, MovieAdmin)
-from django.contrib import admin
+#admin.site.register(Movie, MovieAdmin)
+#from django.contrib import admin
 
-from .models import Movie, Director, Genre, Actor, Comment
+#from .models import Movie, Director, Genre, Actor, Comment
 
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'year', 'director', 'genres_display']
-    list_display_links = ['name']
-    search_fields = ['name']
-    list_filter = ['genres', 'year']
+#class MovieAdmin(admin.ModelAdmin):
+#    list_display = ['id', 'name', 'year', 'director', 'genres_display']
+#    list_display_links = ['name']
+#    search_fields = ['name']
+#    list_filter = ['genres', 'year']
 
 admin.site.register(Director)
 admin.site.register(Genre)
